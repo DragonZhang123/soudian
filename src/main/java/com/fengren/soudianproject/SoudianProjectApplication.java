a13cd0f1794ec8811f5b5e1ade827b6901f43d10
zhang <zgl241@126.com>
2019-03-25 20:05:20 +0800

    first commit

---
+++ src/main/java/com/fengren/soudianproject/SoudianProjectApplication.java
@@ -0,0 +1,13 @@
+package com.fengren.soudianproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoudianProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoudianProjectApplication.class, args);
+	}
+
+}
